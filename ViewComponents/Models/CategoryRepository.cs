48f92b98037652da72c6d19922d0bf272095728a
Mehmet Berkay ŞİRİN <mehmetberkaysirin@aof.anadolu.edu.tr>
2019-11-15 17:19:09 +0300

    change

---
+++ ViewComponents/Models/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponents.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories => throw new NotImplementedException();
+
+        public void AddCategory(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViewComponents/Models/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponents.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => throw new NotImplementedException();
+
+        public void AddProduct(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
