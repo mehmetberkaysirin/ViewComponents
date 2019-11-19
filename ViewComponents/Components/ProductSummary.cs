5f197ec1c1437fd071a6671538e729a4d7cac593
mehmetberkaysirin <mehmetberkaysirin@gmail.com>
2019-11-19 16:35:26 +0300

    ProductSummary

---
+++ ViewComponents/Components/ProductSummary.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewComponents.Models;
+
+namespace ViewComponents.Components
+{
+    public class ProductSummary:ViewComponent
+    {
+        private IProductRepository productRepository;
+
+        public ProductSummary(IProductRepository _productRepository)
+        {
+            productRepository = _productRepository;
+        }
+
+        public string Invoke()
+        {
+            return $"{productRepository.Products.Count()} ürünün toplam fiyatı : {productRepository.Products.Sum(i => i.Price)} TL";
+        }
+    }
+}
